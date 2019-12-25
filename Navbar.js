dade1959a96cea4b8161a2cf5372e931990fe6e5
notme <hanweng9@gmail.com>
2019-12-25 15:37:16 -0500

    sas
    mm

---
+++ Navbar.js
@@ -0,0 +1,21 @@
+
+
+ document.write('\
+ \
+ <link rel="stylesheet" type="text/css" href="background.css"/>\
+\
+<div   class="container">\
+  <div class="topnav">\
+   <nav   >\
+       <ul  >\
+           <li><a href="index.html">About Me</a></li>\
+           <li><a href="projects.html">Projects</a></li>\
+           <li><a href="experiences.html">Experiences</a></li>\
+           <li><a href="blogs/blog.html">Blogs</a></li>\
+       </ul>\
+   </nav>\
+\
+   </div >\
+ </div>\
+ \
+ ');
