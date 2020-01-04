4e741763060a8ae7d9a02dbc85379be8e1236a5f
notme <hanweng9@gmail.com>
2020-01-04 11:49:02 -0500

    d
    d

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 var express = require('express');
+var NoteControl = require('./control/NoteControl');
 var app =express();
 
 app.set('view engine','ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/',function(req,res){
   res.render('index');
 });
 
-app.get('/contact',function(req,res){
-  res.render('project');
+app.get('/experience',function(req,res){
+  res.render('experience');
 });
+app.get('/projects',function(req,res){
+  res.render('projects');
+});
+
+
 
 app.get('/profile/:name',function(req,res){
   res.render('profile',{id: req.params.name});
 });
+NoteControl(app);
 
+
+ 
 app.listen(3000);
