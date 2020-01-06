bc8197350ff0768109e1bccd2fe03b9c9521b261
notme <hanweng9@gmail.com>
2020-01-06 11:38:56 -0500

    h
    h

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var NoteControl = require('./control/NoteControl');
 var app =express();
+var path = require('path');
+
 
 app.set('view engine','ejs');
 app.use('/assets', express.static('assets'));
+app.set('views', [path.join(__dirname,'views' ,'notes'),path.join(__dirname,'views')]);
+
 
 app.get('/',function(req,res){
   res.render('index');
@@ -17,6 +21,16 @@ app.get('/projects',function(req,res){
 });
 
 
+app.get('/MATH1104',function(req,res){
+  res.render('MATH1104');
+});
+
+app.get('/COMP1406',function(req,res){
+  res.render('COMP1406');
+});
+app.get('/COMP2804',function(req,res){
+  res.render('COMP2804');
+});
 
 app.get('/profile/:name',function(req,res){
   res.render('profile',{id: req.params.name});
@@ -24,5 +38,5 @@ app.get('/profile/:name',function(req,res){
 NoteControl(app);
 
 
- 
+
 app.listen(3000);
